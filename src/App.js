0a3789f1b1d440cf54a81c22acc7a7ec3d1eb10b
muthu <mkandanmk7@gmail.com>
2021-09-28 14:43:02 +0530

    Nav done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import "./App.css";
 import Product from "./Components/Product";
 import Nav from "./Components/Nav";
+import { useState } from "react";
 
 function App() {
+  //initialization:
+
+  const [items] = useState([
+    {
+      name: "Toy",
+      price: "₹100",
+      addedToCart: false,
+    },
+    {
+      name: "Mouse",
+      price: "₹700",
+      addedToCart: false,
+    },
+    {
+      name: "Keyboard",
+      price: "₹1500",
+      addedToCart: false,
+    },
+    {
+      name: "HeadPhone",
+      price: "₹9000",
+      addedToCart: false,
+    },
+  ]);
+
+  // cart Count details:
+
+  const [cart, setCart] = useState(0); //by default cart value is 0;
+
   return (
     <div className="App">
+      <Nav cartCount={cart} />
       <Product />
-      <Nav />
     </div>
   );
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,22 @@
 import React from "react";
 
-const Nav = () => {
+const Nav = ({ cartCount }) => {
   return (
-    <div className="App">
-      <h3>hello Nav</h3>
+    <div className="navContainer">
+      <h3>CartioCart</h3>
+      <ul>
+        <li>Home</li>
+        <li>Offer Zone</li>
+        <li>Contact Us</li>
+        <li>About Us</li>
+        <li>Order</li>
+      </ul>
+      <div className="cart">
+        <button>
+          {" "}
+          <strong>Cart</strong> <span className="cartNo">{cartCount}</span>
+        </button>
+      </div>
     </div>
   );
 };
